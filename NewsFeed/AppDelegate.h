d5a3c32bae06d4d5e389a0cf3416f4b60b2887de
ZhangBoxuan <Benny@ZhangBoxuandeMacBook-Pro.local>
2014-10-08 20:08:24 -0400

    Initial Commit

---
+++ NewsFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsFeed
+//
+//  Created by ZhangBoxuan on 14/10/8.
+//  Copyright (c) 2014年 ZhangBoxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
